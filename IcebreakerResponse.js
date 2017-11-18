f5e8d76c45542bab46bb5dc785f18b015d4cd465
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2017-11-18 04:19:50 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -6,10 +6,5 @@ IcebreakerResponse.BatchCreate = function(emails){
       return new IcebreakerResponse
   })
 }
-const emails =[
-  ["avi@flatironschool.com"],
-  ["grace@hopper.com"],
-  ["alan@xparc.com"]
-]
 
 const icebreakerResponses = IcebreakerResponse.BatchCreate(emails)
